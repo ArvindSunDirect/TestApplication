0eeb361cbfe07f3db877c36d96b6b01e1033cd4b
arvindhan.kj <arvindhan.kj@sundirect.in>
2021-10-05 17:01:27 +0530

    button created with added txt

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     String s = "Developing";
 
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.tv);
+        button = findViewById(R.id.btn);
 
         textView.setText("App Developing");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(s);
+            }
+        });
 
 
     }
